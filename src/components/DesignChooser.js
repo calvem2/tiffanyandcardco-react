c301ed6f044bbd3df7d654e737d8987ef7e7da2c
“Megan <calvem2@cs.washington.edu>
2020-08-13 12:34:04 -0700

    Refactor elements rendered into separate functions.

--- src/components/DesignChooser.js
+++ src/components/DesignChooser.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import style from "./Form.css"
 
+const MAX_SELECTIONS = 3; // max number of choice selections allowed
+
 class DesignChooser extends Component {
     // props
     // currentStep: current step in multi-form
-    // formType: type of form (inventory, custom, ready-made)
-    // maxSelections: max number of selections allowed
+    // formType: type of form (inventory, custom, cards)
     // selections: choices selected
     // handleChange: event handler for change to form
 
@@ -23,11 +24,12 @@ class DesignChooser extends Component {
         let images;
         if (this.props.formType === "cards") {
             images = this.importAll(require.context('../card_images/all', false, /\.txt$/));
-        } else if (this.props.formType === "scratch") {
+        } else if (this.props.formType === "custom") {
             images = this.importAll(require.context('../card_images/stamps', false, /\.txt$/));
         } else if (this.props.formType === "inventory") {
             images = this.importAll(require.context('../card_images/inventory', false, /\.txt$/));
         }
+        console.log(images);
         return images[0]['default'].split('\n').filter(x => x);
     };
 
@@ -61,8 +63,7 @@ class DesignChooser extends Component {
         let selections = this.props.selections;
         if (!event.target.checked) {
             selections.delete(event.target.id);
-        } else if (this.props.maxSelections === undefined ||
-                    selections.length < this.props.maxSelections) {
+        } else if (this.props.formType !== "custom" || selections.size < MAX_SELECTIONS) {
             selections.set(event.target.id, {quantity: 1, notes: ""});
             event.target.checked = true;
         } else {
@@ -71,16 +72,15 @@ class DesignChooser extends Component {
         this.props.handleChange({designChoices: selections})
     };
 
-
-    render() {
-        if (this.props.currentStep !== 1) {
-            return null;
-        }
-
-        if (this.props.formType !== "stamps") {
+    /**
+     * Render design options
+     */
+    designOptions = () => {
+        // render on section for card images if not custom request form
+        if (this.props.formType !== "custom") {
             let checkboxes = this.makeCheckboxes(this.getGoogleImages());
             return (
-                <div className={style["form-group"]}>
+                <div className={style["design-sections"]}>
                     <div className={style.section}>
                         <ul>
                             {checkboxes}
@@ -90,6 +90,7 @@ class DesignChooser extends Component {
             );
         }
 
+        // render section for each category of stamps for custom request form
         // sort stamp images into categories
         let stamps = new Map();
         let images = this.getGoogleImages();
@@ -106,7 +107,7 @@ class DesignChooser extends Component {
         for (let [category, images] of stamps) {
             sections.push(
                 <div className={style.section}>
-                    <h2>{category}</h2>
+                    <h3>{category}</h3>
                     <ul>
                         {this.makeCheckboxes(images)}
                     </ul>
@@ -115,10 +116,54 @@ class DesignChooser extends Component {
             )
         }
         return (
-            <div className={style["form-group"]}>
+            <div className={style["design-sections"]}>
+                <h2>STAMPS</h2>
                 {sections}
             </div>
         );
+    };
+
+    /**
+     * Renders section for choosing color preferences
+     */
+    colorChooser = () => {
+        return (
+            <div className={style.section}>
+                <h2>COLORS</h2>
+                <input type="color" value="#e66465"/>
+            </div>
+        );
+    }
+
+    /**
+     * Render short intro message
+     */
+    greeting = () => {
+        if (this.props.formType === "custom") {
+            return (
+                <p className={style["greeting-msg"]}>
+                    Tell me a little bit about what you're looking for!
+                    Select some stamps you vibe with, choose your color color preferences, and
+                    I'll whip something up for you!
+                </p>);
+        }
+        return null;
+    }
+
+    render() {
+        if (this.props.currentStep !== 1) {
+            return null;
+        }
+
+        return (
+            <div className={style["form-group"]}>
+                {/*{this.greeting()}*/}
+                {this.designOptions()}
+                {this.colorChooser()}
+            </div>
+        );
+
+
     }
 }
 
